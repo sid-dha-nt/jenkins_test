0932bdd968bf79873e8feb487d03180c10a1afe7
Siddhant Tripathi <coolvinayak.tripathi83@gmail.com>
2021-05-31 19:35:36 +0530

    Update FizzBuzzTest.java

--- src/test/java/com/techprimers/testing/FizzBuzzTest.java
+++ src/test/java/com/techprimers/testing/FizzBuzzTest.java
@@ -2,46 +2,27 @@ package com.techprimers.testing;
 
 import org.junit.jupiter.api.*;
 
-class FizzBuzzTest {
-
-    public FizzBuzz fB;
-
-    @BeforeEach
-    public void setUp() {
-        fB = new FizzBuzz();
-    }
-
-    @DisplayName("Play FizzBuzz with number = 1")
-    @Test
-    public void testNumber() {
-        String fizzBuzz = fB.play(1);
-        Assertions.assertEquals(fizzBuzz, "1");
-    }
-
-    @DisplayName("Play FizzBuzz with number = 3")
-    @Test
-    public void testFizz() {
-        String fizzBuzz = fB.play(3);
-        Assertions.assertEquals(fizzBuzz, "Fizz");
-    }
-
-    @DisplayName("Play FizzBuzz with number = 5")
-    @Test
-    public void testBuzz() {
-        String fizzBuzz = fB.play(5);
-        Assertions.assertEquals(fizzBuzz, "Buzz");
-    }
-
-    @DisplayName("Don't Play FizzBuzz with number = 0")
-    @Test
-    public void testZero() {
-
-        fB = null;
-    }
-
-    @AfterEach
-    public void tearDown() {
-        fB = null;
-    }
-
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;		
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;		
+public class NewTest {		
+	    private WebDriver driver;		
+		@Test				
+		public void testEasy() {	
+			driver.get("http://demo.guru99.com/test/guru99home/");  
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+		}	
+		@BeforeTest
+		public void beforeTest() {	
+		    driver = new FirefoxDriver();  
+		}		
+		@AfterTest
+		public void afterTest() {
+			driver.quit();			
+		}		
 }
